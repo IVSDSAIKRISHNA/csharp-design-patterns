41676e85e960d17b1a415093e0e8aae109ca62a6
IVSDSAIKRISHNA <ivsdsaikrishna1994@gmail.com>
2026-01-23 23:42:10 +0530

    updated factory pattern

--- CSharpDesignPatterns/Creational Patterns/Factoy/PaymentProcessors/PaymentProcessor.cs
+++ CSharpDesignPatterns/Creational Patterns/Factoy/PaymentProcessors/PaymentProcessor.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace CSharpDesignPatterns.Creational_Patterns.Factoy;
 
+// Actual Factory Class
 public   abstract class PaymentProcessor
 {
     public  abstract IPaymentMethod CreatePaymentMethod();
--- CSharpDesignPatterns/Creational Patterns/Factoy/Payments/CreditCardPayment.cs
+++ CSharpDesignPatterns/Creational Patterns/Factoy/Payments/CreditCardPayment.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CSharpDesignPatterns.Creational_Patterns.Factoy;
+﻿namespace CSharpDesignPatterns.Creational_Patterns.Factoy;
 
 public class CreditCardPayment : IPaymentMethod
 {
     // Method Which Processes the Credit Card Payment.
     public void ProcessPayment(decimal amount)
     {
-        Console.WriteLine($"Credit Card Payment of {amount.ToString("C")} is processed");
+        Console.WriteLine($"Credit Card Payment of {amount} is processed");
     }
 }
