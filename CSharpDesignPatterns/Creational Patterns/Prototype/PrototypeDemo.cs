29d930296e23a606de3a38123e42ace98c9ed04d
IVSDSAIKRISHNA <ivsdsaikrishna1994@gmail.com>
2026-03-10 00:24:26 +0530

    Completed Prototype Example

---
+++ CSharpDesignPatterns/Creational Patterns/Prototype/PrototypeDemo.cs
@@ -0,0 +1,39 @@
+﻿using CSharpDesignPatterns.Creational_Patterns.Prototype.Models;
+
+namespace CSharpDesignPatterns.Creational_Patterns.Prototype;
+
+public static class PrototypeDemo_
+{
+    public static void TestPrototypePattern()
+    {
+        var template = new Document
+        {
+            Title = "Invoice Template",
+            Author = new Author { Name = "Finance Dept" },
+            Metadata = new Metadata { CreatedDate = DateTime.Now }
+        };
+
+        var shallowCopy = template.ShallowClone();
+
+        shallowCopy.Title = "Invoice #101";
+        shallowCopy.Author!.Name = "Accounts Team";
+
+        Console.WriteLine("Original Document:");
+        template.Print();
+
+        Console.WriteLine("Shallow Clone:");
+        shallowCopy.Print();
+
+
+        var deepCopy = template.DeepClone();
+
+        deepCopy.Title = "Invoice #202";
+        deepCopy.Author!.Name = "Billing Team";
+
+        Console.WriteLine("Original Document:");
+        template.Print();
+
+        Console.WriteLine("Deep Clone:");
+        deepCopy.Print();
+    }
+}
